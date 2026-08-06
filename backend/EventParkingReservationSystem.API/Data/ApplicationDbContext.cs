40b09f73d655d0e08d596106106c082b130719c1
CodeEver-CEO <raviraj20517@gmail.com>
2026-08-06 23:39:15 +0530

    Configure customer database and initial authentication migration

--- backend/EventParkingReservationSystem.API/Data/ApplicationDbContext.cs
+++ backend/EventParkingReservationSystem.API/Data/ApplicationDbContext.cs
@@ -1,1 +1,58 @@
-﻿
+﻿using EventParkingReservationSystem.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventParkingReservationSystem.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Customer> Customers => Set<Customer>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.ToTable("Customers");
+
+            entity.HasKey(customer => customer.Id);
+
+            entity.HasIndex(customer => customer.Email)
+                .IsUnique();
+
+            entity.Property(customer => customer.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            entity.Property(customer => customer.Email)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            entity.Property(customer => customer.Phone)
+                .HasMaxLength(20);
+
+            entity.Property(customer => customer.PasswordHash)
+                .IsRequired();
+
+            entity.Property(customer => customer.Status)
+                .HasConversion<string>()
+                .HasMaxLength(30);
+
+            entity.Property(customer => customer.Role)
+                .HasConversion<string>()
+                .HasMaxLength(30);
+
+            entity.Property(customer => customer.EmailVerificationTokenHash)
+                .HasMaxLength(256);
+
+            entity.Property(customer => customer.PasswordResetTokenHash)
+                .HasMaxLength(256);
+        });
+    }
+}
